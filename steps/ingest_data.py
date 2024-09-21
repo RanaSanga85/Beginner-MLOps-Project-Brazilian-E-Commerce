33684cb30df3c71065596b2d925914884d6b960a
RanaSanga85 <rishabhbarman95@gmail.com>
2024-09-21 16:51:24 +0530

    Data Ingestion

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -9,3 +9,19 @@ class IngestData:
     def get_data(self):
         logging.info(f"Ingesting data from {self.data_path}")
         return pd.read_csv(self.data_path)
+    
+@step
+def ingest_data(data_path: str):
+    """
+    Ingesting the data from data_path
+
+    Return:
+       pd.DataFrame of the ingested data
+    """
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(f"Error while ingesting data: {e}")
+        raise e
