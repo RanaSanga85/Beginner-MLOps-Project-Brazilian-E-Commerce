205c294a9335485480e9674a142146cf1edd649a
RanaSanga85 <rishabhbarman95@gmail.com>
2024-09-23 13:17:38 +0530

    train_pipelines

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MLOps Project
+## Readme file
---
+++ run_pipelines.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipelines import train_pipelines
+
+if __name__ == "__main__":
+    train_pipelines(data_path="C:/Users/risha/Desktop/mlops prroject/data/Brazilian E-Commerce Public Dataset by Olist.csv")
