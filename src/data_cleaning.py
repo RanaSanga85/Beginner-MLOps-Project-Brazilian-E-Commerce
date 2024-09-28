f56e0fb7194783b61c1c1d454bcd939719d85615
RanaSanga85 <rishabhbarman10@gmail.com>
2024-09-28 13:12:08 +0530

    data cleaning

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -7,41 +7,43 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-class DataStrategy(ABC):
+class DataHandler(ABC):
     @abstractmethod
-    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+    def process(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
         pass
 
 
-class DataPreprocessStrategy(DataStrategy):
-    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+class PreprocessHandler(DataHandler):
+    """
+    This class implements the handle_data method to perform preprocessing tasks on a DataFrame.
+    """
+    def process(self, data: pd.DataFrame) -> pd.DataFrame:
         try:
-            data=data.drop(
-                [
-                    "order_approved_at",
-                    "order_delivered_carrier_date",
-                    "order_delivered_customer_date",
-                    "order_estimate_delivery_date",
-                    "order_purchase_timestamp",
-                ],
-                axis=1)
-            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
-            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
-            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
-            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
-            data["review_comment_message"].fillna("No review", inplace=True)
+            columns_to_drop = [
+                "order_approved_at", "order_delivered_carrier_date",
+                "order_delivered_customer_date", "order_estimate_delivery_date",
+                "order_purchase_timestamp"
+            ]
+            data = data.drop(columns=columns_to_drop, axis=1)
 
-            data = data.select_dtypes(include=[np.number])
-            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
-            data = data.drop(cols_to_drop, axis=1)
+            # Filling missing values with the median for numerical columns
+            num_cols = ["product_weight_g", "product_length_cm", "product_height_cm", "product_width_cm"]
+            for col in num_cols:
+                data[col].fillna(data[col].median(), inplace=True)
+            
+            # Keep only numerical columns and drop unnecessary ones
+            data = data.select_dtypes(include=[np.number]).drop(["customer_zip_code_prefix", "order_item_id"], axis=1) 
             return data
         except Exception as e:
-            logging.error("Error in preprocessing data: {}".format(e))
+            logging.error(f"Error during data split: {e}")
             raise e
         
 
-class DataSplitStrategy(DataStrategy):
-    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+class SplitHandler(DataHandler):
+    """
+    Splits the dataset into training and testing sets.
+    """
+    def process(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
         
         try:
             X = data.drop(["review_score"], axis=1)
@@ -49,18 +51,22 @@ class DataSplitStrategy(DataStrategy):
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
             return X_train, X_test, y_train, y_test
         except Exception as e:
-            logging.error("Error in splitting data: {}".format(e))
+            logging.error(f"Error during data split: {e}")
             raise e
         
 
-class DataCleaning:
-    def __init__(self, data:pd.DataFrame, strategy:DataStrategy):
+class DataProcessor:
+    def __init__(self, data:pd.DataFrame, strategy:DataHandler):
         self.data = data
-        self.strategy = strategy
+        self.strategy = strategy #An instance of a class that implements the DataStrategy interface.
 
-    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+
+    def execute(self) -> Union[pd.DataFrame, pd.Series]:
+        """
+          Executes the given strategy (preprocessing or splitting).
+        """
         try:
             return self.strategy.handle_data(self.data)
         except Exception as e:
-            logging.error("Error in handling dara: {}".format(e))
+            logging.error(f"Error during data split: {e}")
             raise e
